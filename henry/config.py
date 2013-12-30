0dbff418c60245a09eaeae6e3e5c83c34bd62cb9
Your Name <you@example.com>
2013-12-30 15:22:31 -0500

    fix typo

--- henry/config.py
+++ henry/config.py
@@ -3,24 +3,26 @@ from sqlalchemy import create_engine
 
 
 CONFIG = {
-    'connection_string': 'sqlite:///:memory:',
+    'connection_string': 'mysql+mysqldb://root:no jodas@localhost/henry',
     'echo': True,
+    'ip': 'localhost',
 }
 
 
 def get_engine():
     if not get_engine.engine:
-        engine = create_engine(CONFIG['connection_string'], CONFIG['echo'])
+        print CONFIG
+        engine = create_engine(CONFIG['connection_string'], echo=CONFIG['echo'])
     return get_engine.engine
 get_engine.engine = None
 
 
 def new_session():
-    if not get_session.session:
-        get_session.session_class = sessionmaker(bind=get_engine())
-    return get_session.session_class()
+    if not new_session.session_class:
+        new_session.session_class = sessionmaker(bind=get_engine())
+    return new_session.session_class()
 new_session.session_class = None
 
 
 def create_mysql_string(user, password):
-    return "mysql+mysqldb://%s:%s@localhost/henry" % (user, password)
+    return "mysql+mysqldb://%s:%s@%s/henry" % (user, password, CONFIG['ip'])
