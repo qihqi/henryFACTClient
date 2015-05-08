629d576675e8ccb073ddb66a884c7e1b995f2dd6
han <Han Qi>
2015-05-08 12:33:06 -0500

    simplify wsgi

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,2 @@
 from henry.server import app
-import cred
-from henry import config
-
-connection_string = config.create_mysql_string(cred.U, cred.P)
-config.CONFIG['connection_string'] = connection_string
-config.CONFIG['echo'] = False
 application = app
