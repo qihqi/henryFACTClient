ad32023830f6c749d0d07e90b2f4c8324c7ae42d
han <Han Qi>
2015-05-14 16:36:15 -0500

    beaker optimization

--- henry/authentication.py
+++ henry/authentication.py
@@ -18,24 +18,26 @@ def get_user_info(session, username):
 
 @app.post('/api/authenticate')
 def post_authenticate():
+    beaker = request.environ.get('beaker.session')
+    login_info = beaker.get('login_info')
+    if login_info is not None: # user is already logged in
+        return login_info
+
     username = request.forms.get('username')
     password = request.forms.get('password')
-
     with sessionmanager as session:
         info = get_user_info(session, username)
         if info is None:
             return {'status': False, 'message': 'Usuario no encontrado'}
         if authenticate(username, password, info):
-            session = request.environ.get('beaker.session')
-            session['loginin'] = True
-            session.save()
-            # set session to beaker
-            # return serialized info
-            return {
+            data = {
                 'status': True, 
                 'last_factura': info.last_factura,
                 'bodega_factura_id': info.bodega_factura_id,
             }
+            beaker['login_info'] = data
+            beaker.save()
+            return data
         return {'status': False, 'message': 'Clave equivocada'}
 
 
@@ -43,6 +45,7 @@ def post_authenticate():
 @app.get('/islogin')
 def get_session():
     session = request.environ.get('beaker.session')
-    if not session:
-        print 'not session'
-    return 'answer is ' + str('loginin' in session)
+    login_info = session.get('login_info')
+    if login_info is not None: # user is already logged in
+        return login_info
+    return {'message': 'Not logged in', 'status': False}
