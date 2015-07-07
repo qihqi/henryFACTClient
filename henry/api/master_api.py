5abdaef9044222849a59c0658dd427445874c6ed
han <qihan.dev@gmail.com>
2015-07-06 19:51:44 -0500

    Refactor out modifications on store

--- henry/api/master_api.py
+++ henry/api/master_api.py
@@ -58,6 +58,37 @@ def get_store_by(field, value):
         return canditates[0]
     return None
 
+def fix_inv_by_options(inv, options):
+    inv.items = filter(lambda x: x.cant >= 0, inv.items)
+    inv.meta.paid = True
+    for item in inv.items:
+        if options.usar_decimal:
+            item.cant = Decimal(item.cant)
+        else:
+            # if not using decimal, means that cant is send as int.
+            # treating it as a decimal of 3 decimal places.
+            item.cant = Decimal(item.cant) / 1000
+
+
+    # Get store: if ruc exists get it takes prescendence. Then name, then id.
+    # The reason is that id is mysql autoincrement integer and may not be
+    # consistent across different servers
+    ruc = getattr(inv.meta, 'almacen_ruc', None)  # using None as default value is buggy. Because there could be store with store.ruc == None
+    name = getattr(inv.meta, 'almacen_name', None)
+
+    alm = None
+    if ruc:
+        alm = get_store_by('ruc', ruc)
+    if name and alm is None:
+        alm = get_store_by('nombre', name)
+    if alm is None:
+        alm = prodapi.get_store_by_id(inv.meta.almacen_id)
+
+    inv.meta.almacen_id = alm.almacen_id
+    inv.meta.almacen_name = alm.nombre
+    inv.meta.almacen_ruc = alm.ruc
+    inv.meta.bodega_id = alm.bodega_id
+
 
 @napi.post('/api/nota')
 @dbcontext
@@ -75,16 +106,8 @@ def create_invoice():
         del content['options']
 
     inv = Invoice.deserialize(content)
-    inv.items = filter(lambda x: x.cant >= 0, inv.items)
-    inv.meta.paid = True
+    fix_inv_by_options(inv, options)  # at this point, inv should no longer change
 
-    for item in inv.items:
-        if options.usar_decimal:
-            item.cant = Decimal(item.cant)
-        else:
-            # if not using decimal, means that cant is send as int.
-            # treating it as a decimal of 3 decimal places.
-            item.cant = Decimal(item.cant) / 1000
 
     if options.crear_cliente:  # create client if not exist
         client = inv.meta.client
@@ -96,19 +119,6 @@ def create_invoice():
             prod_id = item.prod.codigo
             if not prodapi.get_producto(prod_id):
                 abort(400, 'Producto con codigo {} no existe'.format(prod_id))
-
-    # Get store: if ruc exists get it takes prescendence. Then name, then id.
-    # The reason is that id is mysql autoincrement integer and may not be
-    # consistent across different servers
-    ruc = getattr(inv.meta, 'almacen_ruc', '$$$')  # using None as default value is buggy. Because there could be store with store.ruc == None
-    name = getattr(inv.meta, 'almacen_name', '$$$')
-    alm = (get_store_by('ruc', ruc) or get_store_by('nombre', name) or
-           prodapi.get_store_by_id(inv.meta.almacen_id))
-
-    inv.meta.almacen_id = alm.almacen_id
-    inv.meta.almacen_name = alm.nombre
-    inv.meta.almacen_ruc = alm.ruc
-    inv.meta.bodega_id = alm.bodega_id
 
     inv = invapi.save(inv)
 
