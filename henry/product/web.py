6c8dd6060de870e3037826043ee64b4c4ae5feb4
han <Han Qi>
2015-12-22 12:58:47 -0500

    Add newprod

--- henry/product/web.py
+++ henry/product/web.py
@@ -1,8 +1,9 @@
 import json
-from bottle import Bottle, request, redirect
-from sqlalchemy.exc import IntegrityError
+from bottle import Bottle, request
+from decimal import Decimal
 
-from .dao import Product, ProdItem, ProdItemGroup, PriceList, PriceListLabel, Inventory
+from .dao import (Product, ProdItem, ProdItemGroup, PriceList,
+    PriceListLabel, Inventory, ProdCount)
 
 
 def convert_to_cent(dec):
@@ -14,6 +15,7 @@ def convert_to_cent(dec):
 def create_full_item_from_dict(
         itemgroupapi, itemapi, priceapi,
         storeapi, bodegaapi, inventoryapi,
+        prodapi, contenidoapi,
         content):
     '''
         input format:
@@ -35,9 +37,27 @@ def create_full_item_from_dict(
 
     itemgroupid = itemgroupapi.create(itemgroup)
 
+    prod = Product()
+    prod.nombre = itemgroup.name
+    prod.codigo = itemgroup.prod_id
+    prodapi.create(prod)
+
     items = {}
     inventories = {}
     allstores = {x.almacen_id: x for x in storeapi.search()}
+
+    conts = {}
+    for bod in bodegaapi.search():
+        if bod.id == -1:
+            continue
+        contenido = ProdCount()
+        contenido.bodega_id = bod.id
+        contenido.prod_id = itemgroup.prod_id
+        contenido.cant = 0
+        contenido.precio = 0
+        contenido.precio2 = 0
+        cid = contenidoapi.create(contenido)
+        conts[bod.id] = cid
 
     for item in content['items']:
         prices = item['prices']
@@ -50,7 +70,7 @@ def create_full_item_from_dict(
         elif i.multiplier > 1:
             i.prod_id = itemgroup.prod_id + '+'
         elif i.multiplier < 1:
-            i.prod_id = itemgroup.prod_id + '+'
+            i.prod_id = itemgroup.prod_id + '-'
 
         item_id = itemapi.create(i)
         items[i.unit] = i
@@ -58,124 +78,52 @@ def create_full_item_from_dict(
         # create bodega
         invs = {}
         for bod in bodegaapi.search():
+            if bod.id == -1:
+                continue
             inv = Inventory()
             inv.item_id = item_id
             inv.bodega_id = bod.id
             inv.cant = 0
             inv_id = inventoryapi.create(inv)
-            invs[bod.id] = inv_id
+            invs[bod.id] = inv
 
         # create prices
         for alm_id, p in prices.items():
             price = PriceList()
             price.nombre = p['display_name']
-            price.precio1 = p['price1']
-            price.precio2 = p['price2']
+            price.precio1 = int(float(p['price1']) * 100)
+            price.precio2 = int(float(p['price2']) * 100)
             price.cant_mayorista = p['cant']
             price.prod_id = i.prod_id
             price.unidad = i.unit
             price.almacen_id = int(alm_id)
             bodid = allstores[price.almacen_id].bodega_id
-            price.upi = invs[bodid]
+            price.upi = conts[bodid]
+            price.multiplicador = i.multiplier
             priceapi.create(price)
 
 
-def make_wsgi_app(dbcontext, auth_decorator,
-                  storeapi, prodapi, priceapi):
-    w = Bottle()
-
-    @w.get('/app/crear_producto')
-    @dbcontext
-    @auth_decorator
-    def create_prod_form(message=''):
-        temp = jinja_env.get_template('prod/crear_producto.html')
-        stores = storeapi.search()
-        categorias = prodapi.category.search()
-        return temp.render(almacenes=stores, categorias=categorias,
-                           message=message)
-
-    @w.get('/app/ver_lista_precio')
-    @dbcontext
-    @auth_decorator
-    def ver_lista_precio():
-        almacen_id = int(request.query.get('almacen_id', 1))
-        prefix = request.query.get('prefix', None)
-        if prefix:
-            prods = priceapi.search(**{'nombre-prefix': prefix,
-                                       'almacen_id': almacen_id})
-        else:
-            prods = []
-        temp = jinja_env.get_template('prod/ver_lista_precio.html')
-        return temp.render(
-            prods=prods, stores=storeapi.search(), prefix=prefix,
-            almacen_name=storeapi.get(almacen_id).nombre)
-
-    @w.post('/app/crear_producto')
-    @dbcontext
-    @auth_decorator
-    def create_prod():
-        p = Product()
-        p.codigo = request.forms.codigo
-        p.nombre = request.forms.nombre
-        p.categoria = request.forms.categoria
-
-        precios = {}
-        for alm in storeapi.search():
-            p1 = request.forms.get('{}-precio1'.format(alm.almacen_id))
-            p2 = request.forms.get('{}-precio2'.format(alm.almacen_id))
-            thres = request.forms.get('{}-thres'.format(alm.almacen_id))
-            if p1 and p2:
-                p1 = convert_to_cent(p1)
-                p2 = convert_to_cent(p2)
-                precios[alm.almacen_id] = (p1, p2, thres)
-        try:
-            prodapi.create_product_full(p, precios)
-            message = 'producto con codigo "{}" creado'.format(p.codigo)
-        except IntegrityError:
-            message = 'Producto con codigo {} ya existe'.format(p.codigo)
-
-        return create_prod_form(message=message)
-
-    @w.get('/app/ver_producto_form')
-    @auth_decorator
-    def ver_producto_form():
-        return jinja_env.get_template('ver_item.html').render(
-            title='Ver Producto',
-            baseurl='/app/producto',
-            apiurl='/api/producto')
-
-    @w.get('/app/producto/<uid>')
-    @dbcontext
-    @auth_decorator
-    def ver_producto(uid):
-        prod = prodapi.get_producto_full(uid)
-        price_dict = {}
-        for x in prod.precios:
-            price_dict[x.almacen_id] = x
-        prod.precios = price_dict
-        temp = jinja_env.get_template('prod/producto.html')
-        return temp.render(prod=prod, stores=storeapi.search())
-
-    @w.get('/app/producto')
-    @dbcontext
-    @auth_decorator
-    def buscar_producto_result():
-        prefix = request.query.prefijo
-        if prefix is None:
-            redirect('/app/ver_producto_form')
-        prods = prodapi.prod.search(**{'nombre-prefix': prefix})
-        temp = jinja_env.get_template('prod/buscar_producto_result.html')
-        return temp.render(prods=prods)
-
-    return w
+def validate_full_item(content, prodapi):
+    prod_id = content['prod']['prod_id']
+    if prodapi.get(prod_id) is not None:
+        return False, 'Codigo ya existe'
+    if len([x for x in content['items'] if int(x['multiplier']) == 1]) != 1:
+        return False, 'Debe haber un unidad con multiplicador 1'
+    all_mult = set()
+    for i in content['items']:
+        if i['multiplier'] in all_mult:
+            return False, 'Todos las unidades debe tener multiplicador distintos'
+        all_mult.add(i['multiplier'])
+    return True, ''
 
 
 def make_wsgi_api(sessionmanager, dbcontext, auth_decorator,
                   storeapi, prodapi, priceapi, itemapi, itemgroupapi,
-                  bodegaapi, inventoryapi):
+                  bodegaapi, inventoryapi, contenidoapi):
 
     app = Bottle()
-    @app.post('/app/item_full')
+
+    @app.post('/app/api/item_full')
     @dbcontext
     @auth_decorator
     def create_item_full():
@@ -191,6 +139,9 @@ def make_wsgi_api(sessionmanager, dbcontext, auth_decorator,
 
         content = request.body.read()
         content = json.loads(content)
+        valid, message = validate_full_item(content, prodapi)
+        if not valid:
+            return {'status': 'failure', 'msg': message}
         create_full_item_from_dict(
             itemgroupapi=itemgroupapi,
             itemapi=itemapi,
@@ -198,12 +149,11 @@ def make_wsgi_api(sessionmanager, dbcontext, auth_decorator,
             storeapi=storeapi,
             bodegaapi=bodegaapi,
             inventoryapi=inventoryapi,
-            content=content
+            prodapi=prodapi,
+            contenidoapi=contenidoapi,
+            content=content,
         )
+        sessionmanager.session.commit()
         return {'status': 'success'}
-
-    @app.get('/static/<rest:path>')
-    def static(rest):
-        return static_file(rest, root='./static/')
 
     return app
