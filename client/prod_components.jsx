e9f67831c953f19667e2ce8c4de85ff317e1e8bc
han <you@example.com>
2016-03-11 08:13:09 -0500

    Add AddItem Table

--- client/prod_components.jsx
+++ client/prod_components.jsx
@@ -2,6 +2,14 @@ import React from 'react';
 import LinkedStateMixin from 'react-addons-linked-state-mixin';
 import ReactDOM from 'react-dom';
 
+export function makeOption(options, name, onchange) {
+    return <select name={name} onChange={onchange}>
+    {options.map((x)=>{
+       return <option value={x.val}>{x.name}</option>;
+    })}
+    </select>;
+}
+
 
 var ProdLoader = React.createClass({
     mixins: [LinkedStateMixin],
@@ -11,7 +19,8 @@ var ProdLoader = React.createClass({
     handleCodigo: function(event) {
         if (event.key == 'Enter') {
             $.ajax({
-                url: '/app/api/itemgroup?prod_id='+encodeURIComponent(this.refs.codigo.value),
+                // url: '/app/api/itemgroup?prod_id='+encodeURIComponent(this.refs.codigo.value),
+                url: '/static/product_test',
                 success: (x) => {
                     var result = JSON.parse(x).result;
                     if (result.length > 0) {
@@ -24,11 +33,13 @@ var ProdLoader = React.createClass({
     },
     handleCant: function(event) {
         if (event.key == 'Enter') {
-            console.log(this.state);
+            this.props.addItem(this.state);
+            this.setState(this.getInitialState());
+            this.refs.codigo.getDOMNode().focus();
         }
     },
     render: function() {
-        return <div className="row"> 
+        return <div className="row">
             <input placeholder="Codigo" ref='codigo' valueLink={this.linkState('prod_id')} onKeyPress={this.handleCodigo}/>
             <input placeholder="Cantidad" ref='cant' valueLink={this.linkState('cant')} onKeyPress={this.handleCant}/>
             {this.state.name}
@@ -43,26 +54,66 @@ var ItemGroupCantList = React.createClass({
         return <table className="table"><tbody>
             {this.props.items.map((c) => {
                 return <tr>
-                    <td>c.prod_id</td>
-                    <td>c.cant</td>
-                    <td>c.name</td>
+                    <td>{c.prod_id}</td>
+                    <td>{c.cant}</td>
+                    <td>{c.name}</td>
                 </tr>;
             })}
         </tbody></table>;
-
-    };
+    }
 });
 
+  var _db_attr = {
+        'uid': 'id',
+        'origin': 'origin',
+        'dest': 'dest',
+        'user': 'user',
+        'trans_type': 'trans_type',
+        'ref': 'ref',
+        'timestamp': 'timestamp',
+        'status': 'status',
+        'value': 'value'};
+
 var App = React.createClass({
+    getInitialState: function() {
+        return {'items': [], meta: {}};
+    },
+    addItem: function(item) {
+        var newItem = {prod: item, cant: item.cant};
+        delete newItem.prod.cant;
+        this.state.items.push(newItem);
+        this.setState({items: this.state.items});
+    },
+    removeItem: function(position) {
+        this.state.items.splice(position, 1);
+        this.setState({items: this.state.items});
+    },
+    addOption: function(op) {
+        this.state.meta[op.target.name] = op.target.value;
+    },
     render: function() {
         return <div className="container">
             <div className="row">
-                <ProdLoader callback={this.addItem} />
+              Desde: {makeOption([{val:1, name:'almacen'}, {val:2, name:'bodega'}], 'origin', this.addOption.bind(this))}
+              Hasta: {makeOption(['almacen', 'bodega'], 'dest', this.addOption.bind(this))}
             </div>
             <div className="row">
+                <ProdLoader addItem={this.addItem} />
+            </div>
+            <div className="row">
+            <table className="table"><tbody>
+                {this.state.items.map((c, index) => {
+                    return <tr>
+                        <td>{c.prod.prod_id}</td>
+                        <td>{c.cant}</td>
+                        <td>{c.prod.name}</td>
+                        <td><button onClick={this.removeItem.bind(this, index)}>Borrar</button></td>
+                    </tr>;
+                })}
+            </tbody></table>;
             </div>
         </div>
     }
 });
 
-ReactDOM.render(<ProdLoader />, document.getElementById('content'));
+ReactDOM.render(<App />, document.getElementById('content'));
