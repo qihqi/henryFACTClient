556b532e8463e4a146ba0fc3dfb5fb4624d4f14e
han <you@example.com>
2016-06-06 16:14:58 -0500

    Add script to create a user

---
+++ tools/create_user.py
@@ -0,0 +1,23 @@
+from hashlib import sha1
+import sys
+from coreapi import dbapi
+from henry.users.dao import User
+
+def main():
+    username = sys.argv[1]
+    password = sys.argv[2]
+
+    user = User()
+    user.username = username
+    s = sha1()
+    s.update(password)
+    user.password = s.hexdigest()
+    user.level = 3
+    user.is_staff = True
+    user.last_factura = 0
+    user.bodega_factura_id = 1
+    with dbapi.session:
+        dbapi.create(user)
+
+if __name__ == '__main__':
+    main()
