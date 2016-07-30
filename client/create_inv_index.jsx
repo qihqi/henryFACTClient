8fd2a0714f7acd304734d031d215abd0089575c5
Han Qi <qihan.dev@gmail.com>
2016-07-30 11:50:27 -0500

    Change to hashhistory

--- client/create_inv_index.jsx
+++ client/create_inv_index.jsx
@@ -2,9 +2,9 @@ import {InvForm} from './create_inv'
 // import {App} from './prod_components'
 import ReactDOM from 'react-dom';
 import React from 'react';
-import {Route, Router, Link, browserHistory} from 'react-router'
+import {Route, Router, Link, hashHistory} from 'react-router'
 
-var router = <Router history={browserHistory}>
+var router = <Router history={hashHistory}>
     <Route name="create_inv" path='/inv' component={InvForm} />
 </Router>;
 //    <Route name="transfer" path='/transfer' component={App} />
